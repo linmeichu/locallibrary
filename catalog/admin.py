8f5eb5237aad616d2bfb3f266a97eee53afbf625
mclin <karenlin1023@gmail.com>
2020-09-30 18:41:03 +0900

    admin page configuration

---
+++ catalog/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Genre)
+
+class BookInline(admin.TabularInline):
+    model = Book
+        
+# Define the admin class
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'display_language')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
